9d13d3a9688bf8439182f6bd8f2c0390e202d268
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-20 13:26:53 -0300

    feat: Go Out NavBar

--- client/src/layouts/NavBar/index.jsx
+++ client/src/layouts/NavBar/index.jsx
@@ -48,7 +48,7 @@ function NavBar() {
                     <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 text-slate-900 text-body font-body transition-all duration-300">
                       <TiInfoLargeOutline className="mr-4 text-lg text-violet-700 hover:text-green-500" />
                       <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
-                        About
+                        Sobre Nosotros
                       </span>
                     </li>
                   </Link>
@@ -56,27 +56,16 @@ function NavBar() {
                     <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 text-slate-900 text-body font-body transition-all duration-300">
                       <FaRegEnvelope className="mr-4 text-lg text-violet-700 hover:text-green-500" />
                       <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
-                        Contact
+                        Contacto
                       </span>
                     </li>
                   </Link>
-                  <div className="absolute inset-y-0 right-0 flex items-center md:hidden pt-2">
-  <button
-    className={`flex flex-col w-6 h-7 border-0 bg-transparent gap-1.5 btn-animation ${
-      isOpen ? "open" : ""
-    }`}
-    onClick={() => setIsOpen(!isOpen)}
-  >
-    <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
-    <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
-    <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
-  </button>
-  <Link to="/">
-    <button className="flex items-center ml-4 text-gray-500 hover:text-gray-900">
-      <IoMdLogOut className="text-xl" />
+                  <Link to="/">
+    <button className="flex items-center ml-4 ">
+      <IoMdLogOut className="mr-3.5 text-gray-500 hover:text-violet-700" />
     </button>
   </Link>
-</div>
+ 
 
                 </ul>
               </div>
@@ -100,14 +89,20 @@ function NavBar() {
               <Link to="/">
                 <li className="flex items-center mr-3 p-3 rounded-full text-slate-900 hover:bg-slate-200 text-body font-body">
                   <TiInfoLargeOutline className="mr-3.5 text-violet-700" />{" "}
-                  About
+                  Sobre Nosotros
                 </li>
               </Link>
               <Link to="/">
                 <li className="flex items-center mr-3 p-3 rounded-full text-slate-900 hover:bg-slate-200 text-body font-body">
-                  <FaRegEnvelope className="mr-3.5 text-violet-700" /> Contact
+                  <FaRegEnvelope className="mr-3.5 text-violet-700" /> Contacto
                 </li>
               </Link>
+                   
+  <Link to="/">
+    <button className="flex items-center ml-4 text-gray-500 hover:text-gray-900">
+      <IoMdLogOut className="mr-3.5 text-gray-500 hover:text-violet-700" />
+    </button>
+  </Link>
             </ul>
           </div>
         </div>
