a7c4bf5140062eceab2b25a1c350b8c67765d509
lucerocamila <lucerogonzalez1997@gmail.com>
2023-04-24 19:23:33 -0300

    style: move perfil div

--- client/src/layouts/NavBar/index.jsx
+++ client/src/layouts/NavBar/index.jsx
@@ -35,8 +35,9 @@ function NavBar() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center md:hidden pt-2">
             <button
-              className={`flex flex-col w-6 h-7 border-0 bg-transparent gap-1.5 btn-animation ${isOpen ? "open" : ""
-                }`}
+              className={`flex flex-col w-6 h-7 border-0 bg-transparent gap-1.5 btn-animation ${
+                isOpen ? "open" : ""
+              }`}
               onClick={() => setIsOpen(!isOpen)}
             >
               <span className="w-full bg-black h-[2px] rounded-md transition-all duration-500 origin-left btn-line"></span>
@@ -52,6 +53,14 @@ function NavBar() {
                     <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 text-slate-900 text-body font-body transition-all duration-300">
                       <IoIosList className="mr-4 text-lg text-violet-700 hover:text-green-500" />
                       Mis listas
+                    </li>
+                  </Link>
+                  <Link>
+                    <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 group text-slate-900 text-body font-body transition-all duration-300">
+                      <BiUserCircle className="mr-4 text-lg text-violet-700 group-hover:text-green-500 transition" />
+                      <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
+                        {/* {decodeToken()?.sub} */} Perfil
+                      </span>
                     </li>
                   </Link>
                   <Link to="/">
@@ -70,21 +79,12 @@ function NavBar() {
                       </span>
                     </li>
                   </Link>
-                  <Link>
-                    <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 group text-slate-900 text-body font-body transition-all duration-300">
-                      <BiUserCircle className="mr-4 text-lg text-violet-700 group-hover:text-green-500 transition" />
-                      <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
-                        {decodeToken()?.sub}
-                      </span>
-                    </li>
-                  </Link>
+
                   <Link to="/">
                     <button className="flex items-center ml-4 ">
                       <IoMdLogOut className="mr-3.5 text-gray-500 hover:text-violet-700" />
                     </button>
                   </Link>
-
-
                 </ul>
               </div>
             </div>
@@ -92,8 +92,9 @@ function NavBar() {
         </div>
 
         <div
-          className={`${isOpen ? "block" : "hidden"
-            } lg:hidden absolute bg-white w-full left-0 z-20`}
+          className={`${
+            isOpen ? "block" : "hidden"
+          } lg:hidden absolute bg-white w-full left-0 z-20`}
         >
           <div className="px-2">
             <ul className="w-full">
@@ -101,6 +102,14 @@ function NavBar() {
                 <li className="flex items-center mr-3 p-3 rounded-full text-slate-900 hover:bg-slate-200 text-body font-body">
                   <IoIosList className="mr-3.5 text-violet-700" />
                   Mis listas
+                </li>
+              </Link>
+              <Link>
+                <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 group text-slate-900 text-body font-body transition-all duration-300">
+                  <BiUserCircle className="mr-4 text-lg text-violet-700 group-hover:text-green-500 transition" />
+                  <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
+                    {/* {decodeToken()?.sub} */} Perfil
+                  </span>
                 </li>
               </Link>
               <Link to="/">
@@ -112,14 +121,6 @@ function NavBar() {
               <Link to="/">
                 <li className="flex items-center mr-3 p-3 rounded-full text-slate-900 hover:bg-slate-200 text-body font-body">
                   <FaRegEnvelope className="mr-3.5 text-violet-700" /> Contacto
-                </li>
-              </Link>
-              <Link>
-                <li className="flex items-center mr-3 p-3 border-b-2 border-transparent hover:border-violet-700 group text-slate-900 text-body font-body transition-all duration-300">
-                  <BiUserCircle className="mr-4 text-lg text-violet-700 group-hover:text-green-500 transition" />
-                  <span className="border-b-2 border-transparent hover:border-gradient-violet-green">
-                    {decodeToken()?.sub}
-                  </span>
                 </li>
               </Link>
 
